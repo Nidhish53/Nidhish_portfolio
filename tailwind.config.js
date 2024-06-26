58feb9076751a88302015e58ac919d9e63d0f485
Nidhish53 <nidhish@gmail.com>
2024-06-26 10:52:45 +0530

    Final Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+    },
+    colors: {
+      lightRed: "#FF6666",
+      lightYellow:"#FFD324",
+      lightPurple:"#6F77F4",
+      lightishGray:"#D8D8D8",
+    }
+  },
+  plugins: [],
+}
+}
